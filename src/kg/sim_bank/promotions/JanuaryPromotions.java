9bfeba585f6197e22e3fb3e84f6ba7fd595f90fb
git <aisuluuisaeva15@gmail.com>
2026-01-14 10:24:55 +0600

    Resolved conflict
    January promo Aisuluu123
    
    USD
    
    Create Aisuluu.txt
    
    January Promotions
    
    January Promotions
    
    1
    
    4

---
+++ src/kg/sim_bank/promotions/JanuaryPromotions.java
@@ -0,0 +1,21 @@
+package kg.sim_bank.promotions;
+
+public class JanuaryPromotions {
+    public static void main(String[] args) {
+
+        System.out.println("January Promotions");
+        System.out.println("January Promotions123");
+        System.out.println("January Promotions1234");
+        System.out.println("January Promotions12345");
+        System.out.println("January Promotions123456");
+        System.out.println("January Promotions1234567");
+
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
+        System.out.println("5");
+        System.out.println("6");
+        System.out.println("7");
+    }
+}
